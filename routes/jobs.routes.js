9a520aa7887915f72ab08d0c5d4b26e02d08fffa
stephan2311 <96991576+stephan2311@users.noreply.github.com>
2022-03-13 17:26:56 +0100

    CRON Job added for fetching Jobs everyday at 10am and 2pm

--- routes/jobs.routes.js
+++ routes/jobs.routes.js
@@ -1,11 +1,16 @@
 const router = require("express").Router();
 const mongoose = require('mongoose');
 const Jobs = require('../models/Jobs.model');
+const nodeCron = require("node-cron");
 
-router.get('/', (req, res, next) => {
-    Jobs.find()
-        .then(allJobs => res.json(allJobs))
-        .catch(err => res.json(err));
+
+const job = nodeCron.schedule("* 10,14 * * *", () => {
+    router.get('/', (req, res, next) => {
+        Jobs.find()
+            .then(allJobs => res.json(allJobs))
+            .catch(err => res.json(err));
+    }); console.log(new Date().toLocaleString());
 });
+
 
 module.exports = router;
